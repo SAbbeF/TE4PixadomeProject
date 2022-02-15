3b1dc116dddc3938620d1a22c5ee9f7e1610a2a6
abbe_F <abdula.fazeli@elev.ga.ntig.se>
2022-02-15 16:47:39 +0100

    Added some basic for player info in lobby

--- TE4Pixadome/Assets/Abdula/Scripts/NetworkManagerLobby.cs
+++ TE4Pixadome/Assets/Abdula/Scripts/NetworkManagerLobby.cs
@@ -1,26 +1,41 @@
 using Mirage;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class NetworkManagerLobby : NetworkManager
 {
+    [SerializeField]
+    private int minimumPlayerCount;
+
     [Scene]
     [SerializeField]
     private string menuScene;
+
+    [SerializeField]
+    private NetworkManager networkManager;
 
     [Header("Room")]
     [SerializeField]
     private NetworkRoomPlayerLobby roomPlayerPrefab;
 
-    public static event Action OnClientConnected;
-    public static event Action OnClientDisconnectedFromServer;
+    public List<NetworkRoomPlayerLobby> RoomPlayers { get; }
+
+    private UnityAction disconnectAction;
+    //public static event Action OnClientConnected;
+    //public static event Action OnClientDisconnectedFromServer;
 
     NetworkManagerLobby()
     {
+        minimumPlayerCount = 2;
+
         menuScene = string.Empty;
         roomPlayerPrefab = null;
+
+        RoomPlayers = new List<NetworkRoomPlayerLobby>();
     }
 
     private void Awake()
@@ -28,16 +43,22 @@ public class NetworkManagerLobby : NetworkManager
         //Identity.OnStartClient.AddListener(OnStartClient);
         //Add a custom method to register all prefabs, can be tracked down from ClientObjectManager, RegisterSpawnPrefabs()
 
-        Client.Authenticated.AddListener(OnClientConnect);
-        Server.Authenticated.AddListener(OnServerConnect);
+        //Client.Authenticated.AddListener(OnClientConnect);
+        //Server.Authenticated.AddListener(OnServerConnect);
+        disconnectAction += OnClientDisconnected;
         //Client.Disconnected.AddListener(OnClientDisconnected);
     }
 
-    private void OnClientConnect(INetworkPlayer conn)
+    private void OnClientDisconnected()
     {
-        //OnClientConnect?.Invoke(this, conn);
-        OnClientConnected?.Invoke();
+        
     }
+
+    //private void OnClientConnect(INetworkPlayer conn)
+    //{
+    //    //OnClientConnect?.Invoke(this, conn);
+    //    OnClientConnected?.Invoke();
+    //}
 
     //private void OnClientDisconnected(ClientStoppedReason arg0)
     //{
@@ -50,8 +71,22 @@ public class NetworkManagerLobby : NetworkManager
     //    OnClientDisconnectedFromServer?.Invoke();
     //}
 
-    private void OnServerConnect(INetworkPlayer conn)
+    //private void OnServerConnect(INetworkPlayer conn)
+    //{
+
+    //}
+
+    public void NotifyPlayersOfReadyState()
+    {
+        foreach (var player in RoomPlayers)
+        {
+            player.HandleReadyToStart(IsReadyToStart());
+        }
+    }
+
+    public bool IsReadyToStart()
     {
 
+        return true;
     }
 }
--- TE4Pixadome/Assets/Abdula/Scripts/NetworkRoomPlayerLobby.cs
+++ TE4Pixadome/Assets/Abdula/Scripts/NetworkRoomPlayerLobby.cs
@@ -1,7 +1,145 @@
 using Mirage;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class NetworkRoomPlayerLobby : NetworkBehaviour
 {
+    [Header("UI")]
+    [SerializeField]
+    private GameObject lobbyUI;
+
+    [SerializeField]
+    private TMP_Text[] playerNameText;
+
+    [SerializeField]
+    private TMP_Text[] playerReadyText;
+
+    [SerializeField]
+    private Button startGameButton;
+
+    [SyncVar(hook = nameof(HandleDisplayNameChanged))]
+    public string displayName;
+
+    [SyncVar(hook = nameof(HandleReadyStatusChanged))]
+    public bool isReady;
+
+    private bool isLeader;
+
+    public bool Isleader
+    {
+        set 
+        {   isLeader = value;
+            startGameButton.gameObject.SetActive(value);
+        }
+    }
+
+    private NetworkManagerLobby lobby;
+
+    //public NetworkManagerLobby NetworkManagerLobby
+    //{
+    //    get
+    //    {
+    //        if (networkManagerLobby != null)
+    //        {
+    //            return networkManagerLobby;
+    //        }
+
+    //        //return NetworkManager.singleton as NetworkManagerLobby;
+    //    }
+    //}
+
+    NetworkRoomPlayerLobby()
+    {
+        lobbyUI = null;
+
+        playerNameText = new TMP_Text[4];
+        playerReadyText = new TMP_Text[4];
+
+        startGameButton = null;
+
+        displayName = "Loading...";
+
+        isReady = false;
+    }
+
+    private void Awake()
+    {
+        //Identity.OnAuthorityChanged.AddListener(OnAu)
+    }
 
 
+    public void HandleDisplayNameChanged(string oldValue, string newValue)
+    {
+        UpdateDisplay();
+    }
+
+    public void HandleReadyStatusChanged(bool oldValue, bool newValue)
+    {
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        if (!HasAuthority)
+        {
+            foreach (var player in lobby.RoomPlayers)
+            {
+                if (player.HasAuthority)
+                {
+                    player.UpdateDisplay();
+                    break;
+                }
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < playerNameText.Length; i++)
+        {
+            playerNameText[i].text = "Waiting For Player...";
+            playerReadyText[i].text = string.Empty;
+        }
+
+        for (int i = 0;i < playerReadyText.Length; i++)
+        {
+            playerNameText[i].text = lobby.RoomPlayers[i].displayName;
+            playerReadyText[i].text = lobby.RoomPlayers[i].isReady ? "<color=green> Ready </color>" : "<color=red> Not Ready </color>";
+        }
+    }
+
+    public void HandleReadyToStart(bool readyToStart)
+    {
+        if (!isLeader)
+        {
+            return;
+        }
+
+        startGameButton.interactable = readyToStart;
+    }
+
+    [ServerRpc]
+    private void CmdSetDisplayName(string displayName)
+    {
+        this.displayName = displayName;
+    }
+
+    [ServerRpc]
+    private void CmdReadyUo()
+    {
+        isReady = !isReady;
+
+        lobby.NotifyPlayersOfReadyState();
+    }
+
+    [ServerRpc]
+    private void CmdStartGame()
+    {
+        //if (lobby.RoomPlayers[0].connectionToClient != connectionToClient)
+        //{
+        //    return;
+        //}
+
+        //Start Game
+    }
 }
