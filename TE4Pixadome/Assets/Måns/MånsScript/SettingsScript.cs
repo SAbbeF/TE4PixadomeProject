ed574bc713f6075b339234b11a48fee83c764ab9
Tofuzz <Luleamons@gmail.com>
2022-03-03 18:03:42 +0100

    2022-03-03
    graphics settings

---
+++ TE4Pixadome/Assets/Måns/MånsScript/SettingsScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsScript : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer;
+    public SettingsScript()
+    {
+
+
+
+    }
+        
+    public void SetVolume(float volume)
+    {
+
+        audioMixer.SetFloat("MasterVolume", volume);
+
+    }
+
+    public void SetGraphics(int dropDownIndex)
+    {
+
+        QualitySettings.SetQualityLevel(dropDownIndex);
+
+    }
+
+
+}
